7d77482aa2e767ca486aa65193b8227274c39fc8
wangfiox <wangfiox@hotmail.com>
2024-05-17 22:13:02 +0800

    :art: 重构了 boss 生成的代码

--- src/edu/hitsz/game/AbstractGame.java
+++ src/edu/hitsz/game/AbstractGame.java
@@ -14,6 +14,7 @@ import edu.hitsz.basic.AbstractFlyingObject;
 import edu.hitsz.aircraft.enemy.EnemyAircraft;
 import edu.hitsz.enemyfactory.BossEnemyFactory;
 import edu.hitsz.enemyfactory.IEnemyAircraftFactory;
+import edu.hitsz.observe.BossNotifier;
 import edu.hitsz.observe.PropGenerator;
 import edu.hitsz.prop.BaseProp;
 import edu.hitsz.observe.EnemyAircraftGenerator;
@@ -107,6 +108,8 @@ public abstract class AbstractGame extends JPanel {
 
     abstract protected void initConfig();
 
+    private final BossNotifier bossNotifier = new BossNotifier();
+
     // TODO 构造函数是 模板函数
     public AbstractGame() {
 
@@ -132,12 +135,6 @@ public abstract class AbstractGame extends JPanel {
     /* ---------- ---------- 模板 end ---------- ---------- */
 
     /**
-     * 这里是 boss
-     */
-    private final static IEnemyAircraftFactory bossFactory = new BossEnemyFactory();
-    private int lastBossScore = 0;
-
-    /**
      * 游戏启动入口，执行游戏逻辑
      */
     public void action() {
@@ -157,7 +154,7 @@ public abstract class AbstractGame extends JPanel {
 
                     // enemyAircrafts.add();
                     // boss 产生器
-                    genBoss();
+                    bossNotifier.update(score, enemyAircrafts);
 
                     // EnemyAircraftGenerator::generateEnemy();
                     enemyAircrafts.add(enemyGenerator.generateEnemy());
@@ -278,28 +275,6 @@ public abstract class AbstractGame extends JPanel {
     private void propMoveAction() {
         for (BaseProp p : props) {
             p.forward();
-        }
-    }
-
-    /**
-     * 这里是同步的 刷新 boss
-     */
-    private void genBoss() {
-        if (score - lastBossScore >= CONFIG.Enemy.BOSS_EVERY_SCORE) {
-            // 保证战场上只有一个 boss
-            boolean existed = enemyAircrafts.stream().anyMatch(x -> (x instanceof BossAircraft));
-            if (!existed) {
-                int locationX = (int) (Math.random()
-                        * (CONFIG.Windows.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())); // 假设游戏宽度为300
-                int locationY = (int) (Math.random() * CONFIG.Windows.WINDOW_HEIGHT * 0.05);
-                int speedX = RANDOM.getRandom(CONFIG.Game.speedX);
-                int speedY = RANDOM.getRandom(CONFIG.Game.speedY); // 假设速度在5到14之间
-                enemyAircrafts
-                        .add(bossFactory.createEnemyAircraft(locationX, locationY, speedX >> 2, speedY >> 2, 500, 100));
-                // 播放音频
-                WaveManager.m_instance.playMusic("bgm_boss");
-                lastBossScore = score;
-            }
         }
     }
 
--- src/edu/hitsz/observe/BossNotifier.java
+++ src/edu/hitsz/observe/BossNotifier.java
@@ -1,49 +1,55 @@
 package edu.hitsz.observe;
 
+import edu.hitsz.aircraft.enemy.BossAircraft;
+import edu.hitsz.aircraft.enemy.EnemyAircraft;
+import edu.hitsz.application.ImageManager;
+import edu.hitsz.application.RANDOM;
+import edu.hitsz.bgm.WaveManager;
 import edu.hitsz.config.CONFIG;
+import edu.hitsz.enemyfactory.BossEnemyFactory;
+import edu.hitsz.enemyfactory.IEnemyAircraftFactory;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class BossNotifier {
 
-    /**
-     * 不出现重复订阅者
-     */
-    private final Set<ISubscriber> m_subers = new HashSet<>();
-
-
     private int score = 0;
 
-    private int lastScore = 0;
+    private int lastBossScore = 0;
 
     public BossNotifier() {
     }
+
+    private final static IEnemyAircraftFactory bossFactory = new BossEnemyFactory();
 
 
     /**
      * @param updataScore
      * @brief 更新分数
      */
-    public void update(int updataScore) {
+    public void update(int updataScore, List<EnemyAircraft> enemyAircrafts) {
         this.score = updataScore;
-        if (score - lastScore >= CONFIG.Enemy.BOSS_EVERY_SCORE) {
+        if (score - lastBossScore >= CONFIG.Enemy.BOSS_EVERY_SCORE) {
             // 保证战场上只有一个 boss
-            lastScore = score;
-//            emitNotify();
-//            this.emitNotify();
-            for (ISubscriber sub : m_subers) {
-                sub.takeNotify();
+            lastBossScore = score;
+            // 保证战场上只有一个 boss
+            boolean existed = enemyAircrafts.stream().anyMatch(x -> (x instanceof BossAircraft));
+            if (!existed) {
+                int locationX = (int) (Math.random()
+                        * (CONFIG.Windows.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())); // 假设游戏宽度为300
+                int locationY = (int) (Math.random() * CONFIG.Windows.WINDOW_HEIGHT * 0.05);
+                int speedX = RANDOM.getRandom(CONFIG.Game.speedX);
+                int speedY = RANDOM.getRandom(CONFIG.Game.speedY); // 假设速度在5到14之间
+                enemyAircrafts
+                        .add(bossFactory.createEnemyAircraft(locationX, locationY, speedX >> 2, speedY >> 2, 500, 100));
+                // 播放音频
+                WaveManager.m_instance.playMusic("bgm_boss");
+                lastBossScore = score;
             }
         }
     }
-
-    public void addSubscriber(ISubscriber sub) {
-        m_subers.add(sub);
-    }
-
-    public void removeSubscriber(ISubscriber sub) {
-        m_subers.remove(sub);
-    }
-
 }
+
+
